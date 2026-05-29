b763275611aa2a867cd10e2f2fa674ef3725e74e
saimohith-24 <chintus2411@gmail.com>
2026-05-29 09:28:37 +0530

    fix: remove ambiguous ContentScale import

--- mobile/app/src/main/java/com/cortisense/app/MainActivity.kt
+++ mobile/app/src/main/java/com/cortisense/app/MainActivity.kt
@@ -30,7 +30,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
